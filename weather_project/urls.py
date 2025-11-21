df5030eca8c66d27461569c4871412929a5b137a
ryusei <konann112x@gmail.com>
2025-11-21 15:16:57 +0900

    Fix health_check import in urls.py

--- weather_project/urls.py
+++ weather_project/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('weather-data/', views.weather_data, name='weather_data'),
     path('weather-graph/', views.weather_graph, name='weather_graph'),
-    path('health/', health_check),
+    path('health/', views.health_check, name='health_check'),
 ]
